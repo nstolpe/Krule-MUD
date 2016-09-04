3018979ed1d4d71e145f6372397b024e49707b7b
nils <slincire@gmail.com>
2016-09-03 22:10:04 -0400

    updated packages. added port option to the server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 'use strict'
 
-let socketio = require('socket.io');
+const http = require('http');
+const socketIO = require('socket.io');
+const program = require('commander');
 
-var io = socketio.listen(3000);
+require('pkginfo')(module, 'version');
+const version = module.exports.version;
 
-console.log('listening on 3000');
+program
+	.version(version)
+	.option('-p, --port [port]', 'port to serve from', 1800)
+	.parse(process.argv);
+
+const server = http.createServer();
+const io = socketIO(server);
 
 io.sockets.on('connection', function(socket) {
 	console.log('connection');
@@ -12,4 +21,7 @@ io.sockets.on('connection', function(socket) {
 		console.log(data);
 		io.sockets.emit('message', data);
 	})
-})
+});
+
+server.listen(program.port);
+console.log('listening on ' + program.port);
