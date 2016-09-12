857b3dfc05c5eafd3cfff7980de7456f08341991
nils <nils@gangshi>
2016-09-12 04:12:42 -0400

    refactored a bit

--- client/hermes.js
+++ client/hermes.js
@@ -1,6 +1,19 @@
 'use strict'
+// hermes = require('./client/hermes')
+// f = hermes.Hub();
+// f.addObserver(f.Observer({receive: function() { console.log(msg) } }, 'foo'))
+// f.addObserver(f.Observer({receive: function(msg) { console.log(msg) } }, 'bar'))
+//
+// f.send(f.Message('bar', 'opening message', ()=>console.log('ggg')))
+
 const events = require('events');
 
+const receiver = {
+    receive: {
+        value: function(msg) {
+        }
+    }
+}
 const hub = {
     /**
      * Initializer that binds the message event.
@@ -9,7 +22,8 @@ const hub = {
         value: function() {
             this.observerKeys = Object.keys(this.Observer(null, null, null));
             this.messageKeys = Object.keys(this.Message(null, null, null));
-            this.on('message', (msg) => this.handleMessage(msg));
+            this.on('message', (msg) => this.broadcast(msg));
+            this.observers = [];
             return this;
         }
     },
@@ -22,55 +36,56 @@ const hub = {
         }
     },
     Observer: {
-        value: function(recipient, message, sender=null) {
-            return { recipient: recipient, message: message, sender: sender };
+        value: function(recipient, messageType, sender=null) {
+            return { recipient: recipient, messageType: messageType, sender: sender };
         }
     },
-    handleMessage: {
+    broadcast: {
         value: function(msg) {
-            console.log(msg);
-            // cb();
+            for (let i = 0, l = this.observers.length; i < l; i++) {
+                if (this.observers[i].messageType === msg.type)
+                    this.observers[i].recipient.receive(msg)
+            }
         }
     },
+    send: {
+        value: function(msg) {
+            this.emit('message', msg);
+        }
+    },
+    /**
+     * Use this to validate messages and observers
+     */
     validateKeys: {
         value: function(incoming, source) {
             return !Object.keys(incoming).filter((el) => source.indexOf(el) < 0).length;
         }
     },
-    addObserver: function(observer) {
-        this.observers.push(observer);
+    addObserver: {
+        value: function(observer) {
+            this.observers.push(observer);
+        }
     },
-    removeObserver: function(observer) {
-        this.observers.splice(this.observers.indexOf(observer));
-    },
-    broadcast: function(message) {
-        let recipients = this.observers.filter(el => el.message === message);
-        console.log(recipients);
+    removeObserver: {
+        value: function(observer) {
+            this.observers.splice(this.observers.indexOf(observer), 1);
+        }
     }
 }
 
 function Hub() {
-    const H = Function.prototype;
+    let H = Object.create(Function.prototype);
     H.prototype = Object.create(events.EventEmitter.prototype, hub);
     return Object.create(H.prototype).init();
 }
-    // Sender() {
-    //     return Object.create(this.sender);
-    // },
-    // sender: {
-    //     send: function(message) {
-    //
-    //     }
-    // },
-    // Receiver() {
-    //     return = Object.create(this.receiver);
-    // },
-    // receiver: {
-    //     receive: function(message) {
-    //
-    //     }
-    // }
+
+function makeReceiver(obj) {
+    let R = Object.create(Function.prototype);
+    R.prototype = Object.create(events.EventEmitter.prototype, receiver);
+    return Object.create(R.prototype);
+}
 
 module.exports = {
-    Hub: Hub
+    Hub: Hub,
+
 };
