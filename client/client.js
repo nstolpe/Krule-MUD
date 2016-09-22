34b13cca3c886a3413b8b60c4caabf9e43f951ee
nils <slincire@gmail.com>
2016-09-22 04:16:27 -0400

    removed unused test message'

--- client/client.js
+++ client/client.js
@@ -13,7 +13,6 @@ Vorpal.use(require('./commands'));
 Vorpal.exec('clear').then(function(data) {
 	Vorpal.log('Welcome to the Krule-MUD client.');
 	Vorpal.log('You are not connected to a server, your options are limited (type `help` if you\'re stuck)');
-	// Socket.sendMessage(Hermes.Message({data: {a: 'l', c:'p'}}));
 });
 
 Vorpal.delimiter('Krule-MUD >>').show();
